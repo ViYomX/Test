f61ca5acbea369b15ea30a8e183648d832b54489
Viyom <151810083+ViYomX@users.noreply.github.com>
2025-05-11 22:30:54 +0530

    Update config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
   cleanUrls: true,
   metaChunk: true,
   editLink: {
-    pattern: 'https://github.com/ViYomX/test/edit/main/docs/:path'
+    pattern: 'https://github.com/ViYomX/test/edit/main/docs/:path',
     text: 'Edit this page on GitHub'
   },
 
