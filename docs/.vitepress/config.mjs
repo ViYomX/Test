9a9d1909a6e7eec32bc91e8606fd4b729ab1fc8a
Viyom <151810083+ViYomX@users.noreply.github.com>
2025-05-11 09:54:46 +0530

    Update config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
   description: "YukkiMusic is a Powerful Telegram Music+Video Bot written in Python using Pyrogram and Py-Tgcalls by which you can stream songs, video and even live streams in your group calls via various sources.",
   head: [
     ['link', { rel: 'icon', type: 'image/svg+xml', href: '/html-favicon.svg' }]
-  ]
+  ],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     logo: '/logo.svg',
